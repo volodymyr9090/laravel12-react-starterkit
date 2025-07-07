9c7202c973f88aba5fc591262407c09e9bc4b0cd
lpsekudus <admsistemlpsekudus@gmail.com>
2025-07-07 10:23:35 +0700

    Clean project and reset history

---
+++ README.md
@@ -0,0 +1,5 @@
+# laravel12-react-starterkit
+
+Starter kit Laravel 12 + React + Inertia.js + Tailwind + ShadCN UI
+
+Lanjut dikembangkan oleh yogijowo
---
+++ resources/js/pages/auth/verify-email.tsx
@@ -0,0 +1,44 @@
+// Components
+import { Head, useForm } from '@inertiajs/react';
+import { LoaderCircle } from 'lucide-react';
+import { FormEventHandler } from 'react';
+
+import TextLink from '@/components/text-link';
+import { Button } from '@/components/ui/button';
+import AuthLayout from '@/layouts/auth-layout';
+
+export default function VerifyEmail({ status }: { status?: string }) {
+    const { post, processing } = useForm({});
+
+    const submit: FormEventHandler = (e) => {
+        e.preventDefault();
+
+        post(route('verification.send'));
+    };
+
+    return (
+        <AuthLayout
+            title="Verify Email"
+            description="Before getting started, verify your email address by clicking on the link we just emailed to you. If you didn't receive the email, we can send you another."
+        >
+            <Head title="Email Verification" />
+
+            {status === 'verification-link-sent' && (
+                <div className="mb-4 text-sm font-medium text-green-600">
+                    A new verification link has been sent to the email address you provided during registration.
+                </div>
+            )}
+
+            <form onSubmit={submit} className="space-y-6 text-center">
+                <Button disabled={processing} variant="secondary">
+                    {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
+                    Resend verification email
+                </Button>
+
+                <TextLink href={route('logout')} method="post" className="mx-auto block text-sm">
+                    Log out
+                </TextLink>
+            </form>
+        </AuthLayout>
+    );
+}
